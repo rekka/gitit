db40095485af38ba1b1db2d3177864cde811d8d0
Norbert Pozar <bertapozar@gmail.com>
2013-06-24 16:58:32 +0900

    Add plugin that adds texmacros.tex as a header

--- Network/Gitit/Plugin/IncludeHeader.hs
+++ Network/Gitit/Plugin/IncludeHeader.hs
@@ -1,12 +1,20 @@
 module Network.Gitit.Plugin.IncludeHeader where
 
+import Control.Monad.CatchIO (try)
 import Network.Gitit.Interface
+import Data.FileStore (FileStoreError, retrieve)
+import Network.Gitit.Framework (filestoreFromConfig)
 
 plugin :: Plugin
 plugin = PreParseTransform addHeader
 
 addHeader :: String -> PluginM String
-addHeader cnt =
-     return ("test\n" ++cnt)
+addHeader cnt = do
+    cfg <- askConfig
+    let fs = filestoreFromConfig cfg
+    texmacros <- try $ liftIO (retrieve fs ("texmacros.tex") Nothing)
+    case texmacros :: Either FileStoreError String of
+          Left  _    -> return cnt
+          Right a    -> return (filter (/= '\r') a ++ "\n\n" ++ cnt)
 
 
